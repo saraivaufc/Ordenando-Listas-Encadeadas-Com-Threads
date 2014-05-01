89a71c24f96f50ac6ce0c1faabd3519a93b958d0
Ciano <cianohanter@hotmail.com>
2014-05-01 03:27:49 -0300

    Update lista.h

--- lista.h
+++ lista.h
@@ -46,48 +46,10 @@ void insere_comeco(Lista * L , int valor){
 	L->tam++;
 	return;
 }
-void insere_fim(Lista * L, int valor){
-	node * novo=(node *) malloc(sizeof(node));
-	novo->valor=valor;
-	novo->prox=NULL;
-	node *aux;
-	for(aux=L->primeiro ; aux->prox != NULL ; aux=aux->prox){};
-	aux->prox=novo;
-	novo->ant=aux;
-	L->tam++;
-	return;
-}
 
 void mostra_lista(Lista *L){
 	for(node *aux=L->primeiro ; aux != NULL ; aux=aux->prox){
 		printf("[%d]",aux->valor);
 	}
 	return;
-}
-
-void remove_lista(Lista *L, int valor){
-	for(node * aux= L->primeiro ; aux != NULL ; aux=aux->prox){
-		if(aux->valor == valor){
-			//verifica se está no começo
-			if(aux->ant == NULL){
-				L->primeiro=aux->prox;
-				aux->prox->ant=NULL;
-				free(aux);
-				L->tam--;
-				return;
-			}
-			//verifica se está no fim
-			if(aux->prox == NULL){
-				aux->ant->prox=NULL;
-				L->tam--;
-				free(aux);
-				return;
-			}
-			aux->ant->prox=aux->prox;
-			aux->prox->ant=aux->ant;
-			L->tam--;
-			free(aux);
-			return;
-		}
-	}
 }
