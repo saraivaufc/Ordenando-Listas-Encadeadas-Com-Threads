d633527245f7296510162ed5498b1de9cb591ba8
Ciano <cianohanter@hotmail.com>
2014-05-23 02:25:25 -0300

    Update lista.h

--- lista.h
+++ lista.h
@@ -1,6 +1,8 @@
 #ifndef LISTA_H
 #define LISTA_H
 
+#include <pthread.h>
+
 typedef struct Nodo_{
 	int valor;
 	struct Nodo_ * prox;
@@ -10,12 +12,14 @@ typedef struct Nodo_{
 typedef struct Lista_{
 	Nodo * primeiro;
 	Nodo * ultimo;
-	int n;
+	int n,n2;
 }Lista;
 
 typedef struct{
 	Lista * L;
 	char * arquivo;
+	int tipo;
+	pthread_mutex_t * mutex;
 }Objeto_Thread;
 
 Lista * Cria_Lista();
