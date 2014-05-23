05d7d89187df185f678ae867e9e297837a5ae981
Ciano <cianohanter@hotmail.com>
2014-05-23 02:24:19 -0300

    Update funcoes.c

--- funcoes.c
+++ funcoes.c
@@ -9,9 +9,11 @@
 
 //minha var globais
 extern int quant_nucleos;
-extern unsigned long long tam_output;
 pthread_mutex_t count_mutex;
-pthread_t threads[quant_arquivos];
+pthread_t threads[quant_arquivos/2];
+
+pthread_mutex_t count_mutex2;
+pthread_t threads2[quant_arquivos/2];
 int saiu_menor=0;
 
 void * carrega_lista_threads(void * obj);
@@ -43,6 +45,8 @@ void swap(int *a, int *b){
 }
 
 void carrega_lista(Lista *L){
+	Lista * L2=Cria_Lista();
+	
 	char nome_arq[22];
 	char arqv[]="arquivos/arquivo";
 	for(int i=0;i<16;i++){
@@ -54,18 +58,34 @@ void carrega_lista(Lista *L){
 	nome_arq[20]='n';
 	nome_arq[21]='\0';
 	
-	printf("Recebendo os arquivos...\n");
-	for(int i=0;i<10;i++){
+	printf("Carregando Arquivos na Lista...\n");
+	for(int i=0;i<quant_arquivos/2;i++){
 		nome_arq[16]=atoi_reverse(i);
 		Objeto_Thread * obj=(Objeto_Thread *)malloc(sizeof(Objeto_Thread));
 		obj->L=L;
 		obj->arquivo=nome_arq;
+		obj->mutex=&count_mutex;
+		
 		pthread_create(&(threads[i]), NULL,carrega_lista_threads, (void *)obj);
 	}
-	
-	for(int i=0; i<quant_arquivos; i++) {
-		pthread_join(threads[i], NULL);
+	for(int i=0;i<quant_arquivos/2;i++){
+		nome_arq[16]=atoi_reverse(i+5);
+		Objeto_Thread * obj=(Objeto_Thread *)malloc(sizeof(Objeto_Thread));
+		obj->L=L2;
+		obj->arquivo=nome_arq;
+		obj->mutex=&count_mutex2;
+
+		pthread_create(&(threads2[i]), NULL,carrega_lista_threads, (void *)obj);
 	}
+	
+	for(int i=0; i<quant_arquivos/2; i++) {
+		pthread_join(threads[i], NULL);
+		pthread_join(threads2[i], NULL);
+	}
+	L->ultimo->prox=L2->primeiro;
+	L2->primeiro->ant=L->ultimo;
+	L->ultimo=L2->ultimo;
+	L->n+=L2->n;
 	printf("Todos os Arquivos Recebidos...\n\n");
 }
 
@@ -81,19 +101,22 @@ void * carrega_lista_threads(void * obj){
 		int valor;
 		fread(&valor, sizeof(valor),1, arq);
 		
-		pthread_mutex_lock(&count_mutex);
-		/* entrando na região critica*/
+		pthread_mutex_lock(Objeto->mutex);
 		Insere(Objeto->L,valor);
-		/* saindo da região critica*/
+		
 		if(feof(arq)){
 			Nodo * aux=Objeto->L->primeiro;
 			Objeto->L->primeiro=Objeto->L->primeiro->prox;
 			Objeto->L->n--;
 			free(aux);
-			pthread_mutex_unlock(&count_mutex);
+
+			pthread_mutex_unlock(Objeto->mutex);
+
 			break;
 		}
-		pthread_mutex_unlock(&count_mutex);
+
+		pthread_mutex_unlock(Objeto->mutex);
+		
 	}
 	
 	fclose(arq);
@@ -101,8 +124,8 @@ void * carrega_lista_threads(void * obj){
 }
 
 Lista * particiona_lista(Lista *L){
-	printf("Tamanho da Lista Geral: %d\n\n",L->n);
-	int pedaco=(int)(L->n-10)/quant_nucleos;
+	printf("Tamanho da Lista Geral: %d\n\n",L->n+L->n2);
+	int pedaco=(int)( L->n-10)/quant_nucleos;
 	Lista * minhas_t=(Lista *) malloc(quant_nucleos * sizeof(Lista));
 	minhas_t[0].primeiro=L->primeiro;
 	minhas_t[0].n=pedaco;
@@ -133,7 +156,7 @@ int count=0;
 
 
 void * ordena_lista_threads(void * arg){
-	printf("\n\t%dª Thread de Ordenacao Criada...\n",++count);
+	printf("\t%dª Thread de Ordenação Criada...\n",++count);
 	Lista *L=(Lista *) arg;
 	ordena(L);
 	return arg;
@@ -148,6 +171,7 @@ void ordena_lista(Lista *Vetor){
 	for(int i=0;i<quant_nucleos;i++){
 		pthread_join(threads_ordenacao[i],NULL);
 	}
+	printf("Entrelaçando as Partes...\n");
 	return;
 }
 
@@ -213,4 +237,27 @@ void cria_saida(Lista * L){
 		fwrite (&menor, sizeof(menor),1, arq);
 	}
 	fclose(arq);
+}
+
+int esta_ordenado(){
+	char path[] = "arquivos/output.bin";
+	FILE *arq = fopen(path, "r");
+	
+	int quant_inteiros=0;
+	int quant_inteiros_des=0;
+	int valor_anterio;
+	int valor_atual;
+	fread(&valor_atual, sizeof(valor_atual),1, arq);
+	quant_inteiros++;
+	
+	while(!(feof(arq))){
+		valor_anterio=valor_atual;
+		fread(&valor_atual, sizeof(valor_atual),1, arq);
+		quant_inteiros++;
+		if(valor_anterio > valor_atual){
+			quant_inteiros_des++;
+		}
+	}
+	fclose(arq);
+	return quant_inteiros_des-1;//retorno um elemento a menos devido a função feof()
 }
